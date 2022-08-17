37235d61154ef426a138e59fa80650f68494bcd8
Fatimah Alghamdi <fatimah-ma@hotmail.com>
2022-08-17 23:58:50 +0300

    Putting it Together Assignment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import PersonCard from './components/PersonCard';
+import React from 'react';
 
 function App() {
+  const [personList , setPersonList] = React.useState(
+    [
+      {id: 1, firstName: "Fatimah", lastName: "Mohammed", age: 10, hairColor: "Brown"},
+      {id: 2, firstName: "Razan", lastName: "Yahya", age: 20, hairColor: "Black"}
+    ]
+  )
+
+  function whenclick(id){
+    setPersonList(personList.map(p => p.id == id ? {...p, age: p.age+1} : p));
+  }
+
   return (
     <div className="App">
-      <PersonCard firstName= "Fatimah" lastName="Alghamdi" age={32} hairColor="Brown"/>
-      <PersonCard firstName= "Razan" lastName="Buraisaly" age={30} hairColor="Black"/>
-      <PersonCard firstName= "Abdulaziz" lastName="Alghamdi" age={24} hairColor="Brown"/>
-      <PersonCard firstName= "Raghad" lastName="Mohammed" age={17} hairColor="Black">
-      <p>I'm a good programmer</p>
-      </PersonCard>
-      <button onClick={ () => alert("This button has been clicked") }>Click here</button>
+    {personList.map((person) => <PersonCard key={person.id}
+        person={person}
+        whenclick={whenclick}
+      />
+    )}
     </div>
   );
 }
--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            thestatus: "on",
+            counter: 0
+        };
+        this.switchlight = this.switchlight.bind(this);
+        this.increaseCounter = this.increaseCounter.bind(this);
+        this.buttonClicked = this.buttonClicked.bind(this);
+    }
+
+    switchlight (){
+        if (this.state.thestatus === "on"){
+            this.setState({thestatus: "off"});
+        }
+        else{
+            this.setState({thestatus: "on"});
+        }
+    }
+
+    increaseCounter(){
+        const afterincrease = this.state.counter +1;
+        const newstateobj = {
+            counter: afterincrease
+        }
+        this.setState(newstateobj);
+    }
+
+    buttonClicked() {
+        console.log("button got clicked in personCard")
+        this.props.whenclick(this.props.person.id);
+    }
+
+
     render(){
         return(
             <div>
-                <h2>{this.props.firstName} {this.props.lastName}</h2>
-                <h5>Age: {this.props.age}</h5>
-                <h5>Hair Color: {this.props.hairColor}</h5>
-                {this.props.children}
+                <h2>{this.props.person.firstName} {this.props.person.lastName}</h2>
+                <h5>Age: {this.props.person.age}</h5>
+                <h5>Hair Color: {this.props.person.hairColor}</h5>
+                {/* <p>The light is currently {this.state.thestatus}</p>
+                <button onClick={this.switchlight}>switch</button>
+                <p>The counter is: {this.state.counter}</p>
+                <button onClick= {this.increaseCounter}>increase counter</button> */}
+
+                <button onClick={this.buttonClicked}>Birthday button</button>
             </div>
         );
     }
